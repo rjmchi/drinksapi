14cb10d2d4721503d75df0ff3b53efa55959233e
rjmchi <robert@rjmprogramming.com>
2020-05-01 15:27:31 -0500

    adds fields for amount and addtional to ingredient

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -7,6 +7,14 @@ const Model = Sequelize.Model;
 class Ingredient extends Model {}
 Ingredient.init({
     // attributes
+    additional: {
+        type: Sequelize.STRING,
+	allowNull: true
+    },
+    amount: {
+        type: Sequelize.STRING,
+	allowNull: true
+    },
     name: {
         type: Sequelize.STRING,
         allowNull: false
