b4fe2f4d8ff08d9ba906227ab23aafb6319c3f99
rjmchi <robert@rjmprogramming.com>
2020-05-01 15:50:02 -0500

    changes lower case port to PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use ('/api', routes);
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, function () {
     console.log(`Listening on port ` + port + `...`);
