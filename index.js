de7a8fe706c03f70633f9efdcba29a7759fb0b1d
rjmchi <robert@rjmprogramming.com>
2020-05-01 15:55:46 -0500

    adds CORS

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const routes = require('./routes/api');
 const app = express();
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header ("Access-Control-Allow-Origin", "*");
+    res.header ("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 app.use ('/api', routes);
 
 const port = process.env.PORT || 3000
